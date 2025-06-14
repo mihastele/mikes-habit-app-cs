091a890a86f4b1c9983c6518b669304ec894bd69
Miha <stele1000@gmail.com>
2025-06-14 16:25:37 +0200

    Reinit MAUI Net8

---
+++ MikeNet8HabitsApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MikeNet8HabitsApp;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ MikeNet8HabitsApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MikeNet8HabitsApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
